d1dd5285574a0172ec774c0637092b8c1fdf8f9c
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-25 14:18:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === "W");
+    
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
